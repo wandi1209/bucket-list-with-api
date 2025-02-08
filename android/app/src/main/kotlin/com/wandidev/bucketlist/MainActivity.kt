ac8227c45cb26881408f368b4db2e1f9fd028312
wandi1209 <wandider@gmail.com>
2025-02-09 04:17:17 +0700

    create project and structure project

---
+++ android/app/src/main/kotlin/com/wandidev/bucketlist/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.wandidev.bucketlist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bucketlist/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: MainScreen(),
+    );
+  }
+}
