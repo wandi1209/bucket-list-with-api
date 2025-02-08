e6917e701766315b91196d8f505d1c7c7ba6cd37
wandi1209 <wandider@gmail.com>
2025-02-09 05:37:36 +0700

    implements api to application

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
@@ -9,19 +11,57 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List<dynamic> bucketListData = [];
+
   Future<void> getData() async {
-    Response response = await Dio().get(
-        "https://flutterapitesting123-default-rtdb.firebaseio.com/bucketlist.json");
-    print(response.data);
+    try {
+      Response response = await Dio().get(
+          "https://flutterapitesting123-default-rtdb.firebaseio.com/bucketlist.json");
+
+      bucketListData = response.data;
+      setState(() {});
+    } catch (exception) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title:
+                  Text("Cannot connect to the server. Try after few second!"),
+            );
+          });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Bucket List")),
-      body: ElevatedButton(
-        onPressed: getData,
-        child: Text("Get Data"),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: getData,
+            child: Text("Get Data"),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: bucketListData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 25,
+                        backgroundImage:
+                            NetworkImage(bucketListData[index]['image'] ?? ""),
+                      ),
+                      title: Text(bucketListData[index]['item'] ?? ""),
+                      trailing:
+                          Text(bucketListData[index]['cost'].toString() ?? ""),
+                    ),
+                  );
+                }),
+          )
+        ],
       ),
     );
   }
