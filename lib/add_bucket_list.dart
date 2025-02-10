a26c6a871ab3a1aa7f4c07b64f93487863f3ed9d
wandi1209 <wandider@gmail.com>
2025-02-10 16:40:42 +0700

    create logic to add data to API

--- lib/add_bucket_list.dart
+++ lib/add_bucket_list.dart
@@ -1,18 +1,68 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class AddBucketList extends StatefulWidget {
-  const AddBucketList({super.key});
+  int newIndex;
+  AddBucketList({super.key, required this.newIndex});
 
   @override
   State<AddBucketList> createState() => _AddBucketListState();
 }
 
 class _AddBucketListState extends State<AddBucketList> {
+  TextEditingController nameText = TextEditingController();
+  TextEditingController costText = TextEditingController();
+  TextEditingController imageURL = TextEditingController();
+  Future<void> addData() async {
+    try {
+      Map<String, dynamic> data = {
+        "item": nameText.text,
+        "cost": costText.text,
+        "image": imageURL.text,
+        "complete": false
+      };
+      Response response = await Dio().patch(
+          "https://flutterapitesting123-default-rtdb.firebaseio.com/bucketlist/${widget.newIndex}.json",
+          data: data);
+      Navigator.pop(context, "refresh");
+      // ignore: empty_catches
+    } catch (e) {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Add Bucket List"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameText,
+              decoration: InputDecoration(label: Text("Name Bucket List")),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: costText,
+              decoration: InputDecoration(label: Text("Estimated Cost")),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: imageURL,
+              decoration: InputDecoration(label: Text("Image URL")),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                    child: ElevatedButton(
+                        onPressed: addData, child: Text("Submit"))),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -63,11 +63,11 @@ class _MainScreenState extends State<MainScreen> {
         : ListView.builder(
             itemCount: bucketListData.length,
             itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: (bucketListData[index] is Map &&
-                        !bucketListData[index]?['complete'])
-                    ? ListTile(
+              return (bucketListData[index] is Map &&
+                      !bucketListData[index]?['complete'])
+                  ? Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
                         onTap: () {
                           Navigator.push(
                             context,
@@ -94,9 +94,9 @@ class _MainScreenState extends State<MainScreen> {
                         title: Text(bucketListData[index]?['item'] ?? ""),
                         trailing: Text(
                             bucketListData[index]?['cost'].toString() ?? ""),
-                      )
-                    : SizedBox(),
-              );
+                      ),
+                    )
+                  : SizedBox();
             },
           );
   }
@@ -107,8 +107,14 @@ class _MainScreenState extends State<MainScreen> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return AddBucketList();
-          }));
+            return AddBucketList(
+              newIndex: bucketListData.length,
+            );
+          })).then((value) {
+            if (value == "refresh") {
+              getData();
+            }
+          });
         },
         shape: CircleBorder(),
         child: Icon(Icons.add),
